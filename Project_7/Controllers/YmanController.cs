d727cb81d5d37df4e2b7c4e3a6aa055be336ad22
Yaman Khashashneh <ymankh1997@gmail.com>
2024-09-06 14:41:22 +0300

    Add new controller

---
+++ Project_7/Controllers/YmanController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project_7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YmanController : ControllerBase
+    {
+    }
+}
