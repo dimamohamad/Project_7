f60f71d9f18cf9c04b134b4301cee84aba7b1e49
ahmed-yacoub <ahmedyacoub718@gmail.com>
2024-09-06 20:52:44 +0300

    RegisterUser

---
+++ Project_7/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Project_7.DTOs;
+using Project_7.Models;
+
+namespace Project_7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+        public UsersController(MyDbContext db) 
+        {
+            _db = db;
+        }
+        [HttpPost ("RegisterUsers")]
+        public IActionResult Register([FromForm] UserRegisterDTO user)
+        {
+            byte[] hash;
+            byte[] salt;
+            PasswordHash.Hasher(user.Passwword, out hash, out salt);
+            var data = new User
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Passwword = user.Passwword,
+                PasswordHash = hash,
+                PasswordSalt = salt,
+            };
+            return Ok(data);
+        }
+    }
+}
---
+++ Project_7/DTOs/PasswordHash.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Project_7.DTOs
+{
+    public class PasswordHash
+    {
+        public static void Hasher(string password, out byte[] passwordhash ,out byte[] passwordsalt)
+        {
+            using (var l = new System.Security.Cryptography.HMACSHA512())
+            {
+                
+                passwordsalt = l.Key;
+                passwordhash = l.ComputeHash(Encoding.UTF8.GetBytes(password)); //trans from string to byte
+
+            }
+        }
+    }
+}
