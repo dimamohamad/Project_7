04f23c23f8cd02a8fe8e0db672eb1683c8c4ccbd
Raneem-Alhalahlah <raneemalhalahleh@gmail.com>
2024-09-08 21:04:43 +0300

    add navbar to all page

--- Project_7/Front-end/js/contact.js
+++ Project_7/Front-end/js/contact.js
@@ -15,3 +15,30 @@ async function sendMessage() {
     alert("Failed to send message");
   }
 }
+
+async function GetCategories() {
+  var response = await fetch(
+    "https://localhost:44339/API/Categories/GetAllCategories"
+  );
+  var result = await response.json();
+  var container = document.getElementById("dropDownCategory");
+  result.forEach((element) => {
+    container.innerHTML += `
+             <li>
+                    <a
+                      class="block px-6 py-2 transition-all duration-300 hover:text-primary-500"
+                      href="shop-grid.html"
+                      onclick="storeCategoryId(${element.categoryId})"
+                    >
+                       ${element.categoryName}
+                    </a>
+                  </li>
+            
+    `;
+  });
+}
+function storeCategoryId(categoryId) {
+  localStorage.setItem("CategoryId", categoryId);
+  window.location.href = "shop-grid.html";
+}
+GetCategories();
