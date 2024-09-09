a1709948775cc13af99f8a62f2733bd6c5c87d06
ahmed-yacoub <ahmedyacoub718@gmail.com>
2024-09-09 22:20:17 +0300

    delete test.js

--- Project_7/Front-end/js/test.js
+++
@@ -1,6 +0,0 @@
-function ClearLocalStorage(event) {
-  event.preventDefault();
-  localStorage.removeItem("Token");
-  localStorage.removeItem("userId");
-  window.location.href = "sign-in.html";
-}
