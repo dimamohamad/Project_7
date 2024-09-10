9e9accea29f3590f3a09d203f92dd040220ee0b3
NourShloul <shloulnour31@gmail.com>
2024-09-10 23:16:31 +0300

    add edit voucher in admin plus edit styling

--- Project_7/Admin-Front/assets/js/showVoucher.js
+++ Project_7/Admin-Front/assets/js/showVoucher.js
@@ -25,7 +25,7 @@ async function GetAllVouchers() {
                       <td class="time align-middle white-space-nowrap text-body-tertiary text-opacity-85 ps-4">${voucher.endDate}</td>
                       <td class="align-middle white-space-nowrap text-end pe-0 ps-4 btn-reveal-trigger">
                         <div class="btn-reveal-trigger position-static"><button class="btn btn-sm dropdown-toggle dropdown-caret-none transition-none btn-reveal fs-10" type="button" data-bs-toggle="dropdown" data-boundary="window" aria-haspopup="true" aria-expanded="false" data-bs-reference="parent"><span class="fas fa-ellipsis-h fs-10"></span></button>
-                          <div class="dropdown-menu dropdown-menu-end py-2"><a class="dropdown-item" href="UpdateCategory.html" onclick="UpdateVoucher(${voucher.voucherId})">Edit </a>
+                          <div class="dropdown-menu dropdown-menu-end py-2"><a class="dropdown-item" href="UpdateVoucher.html" onclick="UpdateVoucher(${voucher.voucherId})">Edit </a>
                             <div class="dropdown-divider"></div><a onclick="DeleteVoucher(${voucher.voucherId})" class="dropdown-item text-danger" href="#!">Remove</a>
                           </div>
                         </div>
@@ -43,9 +43,9 @@ async function GetAllVouchers() {
 
 //       };
 
-function UpdateVoucher(id) {
+function setIdVoucher(id) {
   localStorage.setItem("Voucher", id);
-  window.location.href = "UpdateCategory.html";
+  window.location.href = "showVoucher.html";
 }
 
 async function DeleteVoucher(VoucherId) {
@@ -58,3 +58,30 @@ async function DeleteVoucher(VoucherId) {
   window.location.reload();
 }
 GetAllVouchers();
+
+async function UpdateVoucher() {
+  const n = localStorage.getItem("Voucher");
+  var url = `https://localhost:44339/api/Vouchers/${n}`;
+  let voucherCode = document.getElementById("voucherCode").value;
+  let discountPercentage = document.getElementById("discountPercentage").value;
+  let startDate = document.getElementById("startDate").value;
+  let endDate = document.getElementById("endDate").value;
+  let isActive = document.getElementById("isActive").value;
+  var data = {
+    voucherCode: voucherCode,
+    discountPercentage: discountPercentage,
+    startDate: startDate,
+    endDate: endDate,
+    isActive: isActive,
+  };
+  event.preventDefault();
+  let response = await fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  alert("Voucher Updated successfully ");
+  window.location.href("showVoucher.html");
+}
