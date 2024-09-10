459ca1de387c816348156beff466c579161b1966
dimamohamad <deemaahmad189@gmail.com>
2024-09-10 22:20:45 +0300

    remove comment on chat controller

--- Project_7/Controllers/ChatController.cs
+++ Project_7/Controllers/ChatController.cs
@@ -1,34 +1,34 @@
 ﻿
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.DotNet.Scaffolding.Shared.Messaging;
-//using System;
-//namespace Project_7.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class ChatController : ControllerBase
-//    {
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using System;
+namespace Project_7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
 
 
 
-//        private static List<Project_7.Models.Message> Messages = new List<Project_7.Models.Message>();
+        private static List<Project_7.Models.Message> Messages = new List<Project_7.Models.Message>();
 
-//        // Get all messages
-//        [HttpGet]
-//        public IActionResult GetMessages()
-//        {
-//            return Ok(Messages.OrderBy(m => m.Timestamp));
-//        }
+        // Get all messages
+        [HttpGet]
+        public IActionResult GetMessages()
+        {
+            return Ok(Messages.OrderBy(m => m.Timestamp));
+        }
 
-//        // Post a new message
-//        [HttpPost]
-//        public IActionResult PostMessage([FromBody] Project_7.Models.Message message)
-//        {
-//            message.Id = Messages.Count + 1;
-//            message.Timestamp = DateTime.UtcNow;
-//            Messages.Add(message);
-//            return Ok(message);
-//        }
-//    }
-//}
+        // Post a new message
+        [HttpPost]
+        public IActionResult PostMessage([FromBody] Project_7.Models.Message message)
+        {
+            message.Id = Messages.Count + 1;
+            message.Timestamp = DateTime.UtcNow;
+            Messages.Add(message);
+            return Ok(message);
+        }
+    }
+}
