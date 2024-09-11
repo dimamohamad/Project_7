54c087363e865e98a5639be0d52e2ec34bf29c69
NourShloul <shloulnour31@gmail.com>
2024-09-11 15:05:20 +0300

    edit at update product js

--- Project_7/Admin-Front/assets/js/UpdateProduct.js
+++ Project_7/Admin-Front/assets/js/UpdateProduct.js
@@ -29,11 +29,21 @@ async function getCategoryName() {
 
 async function getProduct() {
   let n = localStorage.getItem("productId");
-  var url = `https://localhost:44338/Api/Products/UpdateOnproduct/${n}`;
+  var url = `https://localhost:44338/Api/Products/GetProductsById/${n}`;
   let response = await fetch(url);
   let result = await response.json();
   document.getElementById("productName").value = result.productName;
   document.getElementById("Description").value = result.description;
-  document.getElementById("Price").value = result.categoryImage;
+  document.getElementById("Price").value = result.price;
+  document.getElementById("ProductImage1").value = result.ProductImage1;
+  document.getElementById("ProductImage2").value = result.ProductImage2;
+  document.getElementById("ProductImage3").value = result.ProductImage3;
+  document.getElementById("ProductImage4").value = result.ProductImage4;
+  document.getElementById("ProductImage5").value = result.ProductImage5;
+  document.getElementById("ProductImage6").value = result.ProductImage6;
+  document.getElementById("Visiblity").value = result.Visiblity;
+  document.getElementById("StockQuantity").value = result.StockQuantity;
+  document.getElementById("DiscountPercentage").value =
+    result.DiscountPercentage;
 }
 getProduct();
