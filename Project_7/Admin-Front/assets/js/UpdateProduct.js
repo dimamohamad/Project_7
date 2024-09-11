4eb5f06fb173487b3498579737d1179a0fa5a78f
NourShloul <shloulnour31@gmail.com>
2024-09-11 12:18:56 +0300

    edited select product

--- Project_7/Admin-Front/assets/js/UpdateProduct.js
+++ Project_7/Admin-Front/assets/js/UpdateProduct.js
@@ -1,18 +1,39 @@
 const n = localStorage.getItem("productId");
-var url =` https://localhost:44338/Api/Products/UpdateOnproduct/${n}`;
+var url = `https://localhost:44338/Api/Products/UpdateOnproduct/${n}`;
 var form = document.getElementById("UpdateProductForm");
 async function UpdateProductform() {
+  var formData = new FormData(form);
+  event.preventDefault();
+  let response = await fetch(url, {
+    method: "PUT",
+    body: formData,
+  });
 
-    var formData = new FormData(form);
-    event.preventDefault();
-    let response = await fetch(url, {
-        method: 'PUT',
-        body: formData,
-    });
-
-
-
-    var data = response;
-     window.location.href = "products.html"
-   alert("your product has successfully edited");
+  var data = response;
+  window.location.href = "products.html";
+  alert("your product has successfully edited");
 }
+
+async function getCategoryName() {
+  const dropDown = document.getElementById("dropDownList");
+  let url = "https://localhost:44338/API/Categories/GetAllCategories";
+  let request = await fetch(url);
+  let data = await request.json();
+
+  data.forEach((select) => {
+    dropDown.innerHTML += (
+      <option value="${select.categoryId}">${select.categoryName}</option>
+    );
+  });
+}
+
+async function getProduct() {
+  let n = localStorage.getItem("productId");
+  var url = `https://localhost:44338/Api/Products/UpdateOnproduct/${n}`;
+  let response = await fetch(url);
+  let result = await response.json();
+  document.getElementById("productName").value = result.productName;
+  document.getElementById("Description").value = result.description;
+  document.getElementById("Price").value = result.categoryImage;
+}
+getProduct();
