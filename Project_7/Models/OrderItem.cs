b3f6fb400d917e0481ff2452c2a61d01949573ae
Raneem-Alhalahlah <raneemalhalahleh@gmail.com>
2024-09-15 10:12:53 +0300

    aa

--- Project_7/Models/OrderItem.cs
+++ Project_7/Models/OrderItem.cs
@@ -13,5 +13,9 @@ public partial class OrderItem
 
     public decimal TotalPrice { get; set; }
 
+    public int? ProductId { get; set; }
+
     public virtual Order? Order { get; set; }
+
+    public virtual Product? Product { get; set; }
 }
